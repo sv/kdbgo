883b0b958cd8ed39434f51ee50bae3b3da328340
Sergey Vidyuk <svidyuk@gmail.com>
2013-06-10 01:44:04 +0100

    more test. basic connectivity support

--- kdb.go
+++ kdb.go
@@ -2,14 +2,74 @@ package kdb
 
 import (
 	"bufio"
-	//"bytes"
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/nu7hatch/gouuid"
 	"io"
+	"net"
 	"time"
 )
+
+type KDBConn struct {
+	con     *net.TCPConn
+	Host    string
+	Port    string
+	userpwd string
+}
+
+func (c *KDBConn) Close() error {
+	return c.con.Close()
+}
+
+func (c *KDBConn) Cmd(cmd string) (data interface{}, err error) {
+	var order = binary.LittleEndian
+	cmdbuf := new(bytes.Buffer)
+	binary.Write(cmdbuf, order, int8(10))
+	binary.Write(cmdbuf, order, int8(0))
+	binary.Write(cmdbuf, order, int32(len(cmd)))
+	binary.Write(cmdbuf, order, []byte(cmd))
+
+	msglen := int32(8 + len(cmdbuf.Bytes()))
+	var header = ipcHeader{1, 1, 0, 0, msglen}
+	buf := new(bytes.Buffer)
+	err = binary.Write(buf, order, header)
+	err = binary.Write(buf, order, cmdbuf.Bytes())
+	_, err = c.con.Write(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return Decode(c.con)
+}
+func DialKDB(host string, port int, auth string) (*KDBConn, error) {
+	tcpaddr, err := net.ResolveTCPAddr("tcp", host+":"+fmt.Sprint(port))
+	if err != nil {
+		return nil, err
+	}
+	//fmt.Println("connecting")
+	conn, err := net.DialTCP("tcp", nil, tcpaddr)
+	if err != nil {
+		return nil, err
+	}
+	// handshake - assuming latest protocol
+	var buf = bytes.NewBufferString(auth)
+	buf.WriteByte(0)
+	_, err = conn.Write(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	var reply = make([]byte, 2+len(auth))
+	n, err := conn.Read(reply)
+	if err != nil {
+		return nil, err
+	}
+	if n != 1 {
+		return nil, errors.New("Authentication error" + string(reply))
+	}
+	kdbconn := KDBConn{conn, host, string(port), auth}
+	return &kdbconn, nil
+}
 
 var ErrBadMsg = errors.New("Bad Message")
 var ErrBadHeader = errors.New("Bad header")
@@ -43,7 +103,7 @@ type ipcHeader struct {
 	ByteOrder   byte
 	RequestType byte
 	Compressed  byte
-	_           byte
+	Reserved    byte
 	MsgSize     int32
 }
 
@@ -58,11 +118,12 @@ func (h *ipcHeader) getByteOrder() binary.ByteOrder {
 func Decode(src io.Reader) (kobj interface{}, e error) {
 	var r = bufio.NewReader(src)
 	var header ipcHeader
+	//fmt.Println("Reading header")
 	err := binary.Read(r, binary.LittleEndian, &header)
 	if err != nil {
 		fmt.Println("binary.Read failed:", err)
 	}
-	fmt.Println("Header -> ", header)
+	//fmt.Println("Header -> ", header)
 	var order = header.getByteOrder()
 	return readData(r, order)
 }
@@ -70,7 +131,7 @@ func readData(r *bufio.Reader, order binary.ByteOrder) (kobj interface{}, err er
 	var msgtype int8
 	//var msglen = header.MsgSize
 	binary.Read(r, order, &msgtype)
-	fmt.Println("Msg Type:", msgtype)
+	//fmt.Println("Msg Type:", msgtype)
 	switch msgtype {
 	case -1:
 		var b byte
@@ -126,6 +187,23 @@ func readData(r *bufio.Reader, order binary.ByteOrder) (kobj interface{}, err er
 			return string(arr.([]byte)), nil
 		}
 		return arr, nil
+	case 0:
+		var vecattr int8
+		binary.Read(r, order, &vecattr)
+		var veclen int32
+		err = binary.Read(r, order, &veclen)
+		if err != nil {
+			fmt.Println("Reading vector length failed -> %v", err)
+		}
+		var arr = make([]interface{}, veclen)
+		for i := 0; i < int(veclen); i++ {
+			v, err := readData(r, order)
+			if err != nil {
+				return nil, err
+			}
+			arr[i] = v
+		}
+		return arr, nil
 	case 11:
 		var vecattr int8
 		binary.Read(r, order, &vecattr)
@@ -143,6 +221,37 @@ func readData(r *bufio.Reader, order binary.ByteOrder) (kobj interface{}, err er
 			arr[i] = string(line[:len(line)-1])
 		}
 		return arr, nil
+	case 99, 127:
+		var dict struct{ K, V interface{} }
+		k, err := readData(r, order)
+		if err != nil {
+			return nil, err
+		}
+		dict.K = k
+		v, err := readData(r, order)
+		if err != nil {
+			return nil, err
+		}
+		dict.V = v
+		return dict, nil
+	case 98:
+		var vecattr int8
+		binary.Read(r, order, &vecattr)
+		return readData(r, order)
+
+	case 100:
+		var f struct{ Namespace, Body string }
+		line, err := r.ReadBytes(0)
+		if err != nil {
+			return nil, err
+		}
+		f.Namespace = string(line[:len(line)-1])
+		b, err := readData(r, order)
+		if err != nil {
+			return nil, err
+		}
+		f.Body = b.(string)
+		return f, nil
 	case -128:
 		line, err := r.ReadBytes(0)
 		if err != nil {
