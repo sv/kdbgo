55db9760420bed01b06f11f3baab2bede27bcab6
Sergey Vidyuk <svidyuk@gmail.com>
2013-09-12 22:57:15 +0100

    add examples

---
+++ example_test.go
@@ -0,0 +1,20 @@
+package kdb_test
+
+import (
+	"fmt"
+	"github.com/sv/kdbgo"
+)
+
+func ExampleKDBConn_Call() {
+	con, err := kdb.DialKDB("localhost", 1234, "")
+	if err != nil {
+		fmt.Println("Failed to connect:", err)
+		return
+	}
+
+	res, err := con.Call("til", int32(10))
+	if err != nil {
+		fmt.Println("Query failed:", err)
+	}
+	fmt.Println("Result:", res)
+}
