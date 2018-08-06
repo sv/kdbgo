748227358271748514e2cbbbb79e0c85504d5c8d
Sergey Vidyuk <svidyuk@gmail.com>
2018-08-06 17:57:15 +0100

    add more tests

--- encode_test.go
+++ encode_test.go
@@ -17,23 +17,23 @@ var encodingTests = []struct {
 	{"0b", &K{-KB, NONE, false}, BoolBytes},
 	{"01b", &K{KB, NONE, []bool{false, true}}, BoolVecBytes},
 	{"1i", Int(1), IntBytes},
-	{"enlist 1i", &K{KI, NONE, []int32{1}}, IntVectorBytes},
+	{"enlist 1i", IntV([]int32{1}), IntVectorBytes},
 	{"`byte$til 5", &K{KG, NONE, []byte{0, 1, 2, 3, 4}}, ByteVectorBytes},
 	{"\"GOOG\"", &K{KC, NONE, "GOOG"}, CharArrayBytes},
-	{"`GOOG", &K{-KS, NONE, "GOOG"}, SymbolBytes},
+	{"`GOOG", Symbol("GOOG"), SymbolBytes},
 	{"`abc`bc`c", SymbolV([]string{"abc", "bc", "c"}), SymbolVectorBytes},
-	{"`a`b!2 3", NewDict(SymbolV([]string{"a", "b"}), &K{KI, NONE, []int32{2, 3}}), DictWithAtomsBytes},
+	{"`a`b!2 3", NewDict(SymbolV([]string{"a", "b"}), IntV([]int32{2, 3})), DictWithAtomsBytes},
 	{"{x+y} in .d", NewFunc("d", "{x+y}"), FuncNonRootBytes},
 	{"{x+y}", NewFunc("", "{x+y}"), FuncBytes},
 	{"'type", Error(errors.New("type")), ErrorBytes},
 	{"(\"ac\";`b;`)", NewList(&K{KC, NONE, "ac"}, Symbol("b"), Symbol("")), GenericList2Bytes},
-	{"`byte$enlist til 5", &K{K0, NONE, []*K{{KG, NONE, []byte{0, 1, 2, 3, 4}}}}, GeneralListBytes},
+	{"`byte$enlist til 5", NewList([]*K{{KG, NONE, []byte{0, 1, 2, 3, 4}}}...), GeneralListBytes},
 	{"([]a:enlist 2;b:enlist 3)", NewTable([]string{"a", "b"},
-		[]*K{{KI, NONE, []int32{2}}, {KI, NONE, []int32{3}}}),
+		[]*K{IntV([]int32{2}), IntV([]int32{3})}),
 		TableBytes},
-	{"([a:enlist 2i]b:enlist 3i)", NewDict(NewTable([]string{"a"}, []*K{{KI, NONE, []int32{2}}}), NewTable([]string{"b"}, []*K{{KI, NONE, []int32{3}}})), KeyedTableBytes},
+	{"([a:enlist 2i]b:enlist 3i)", NewDict(NewTable([]string{"a"}, []*K{IntV([]int32{2})}), NewTable([]string{"b"}, []*K{IntV([]int32{3})})), KeyedTableBytes},
 	{"`a`b!enlist each 2 3", NewDict(SymbolV([]string{"a", "b"}),
-		&K{K0, NONE, []*K{{KI, NONE, []int32{2}}, {KI, NONE, []int32{3}}}}),
+		NewList([]*K{IntV([]int32{2}), IntV([]int32{3})}...)),
 		DictWithVectorsBytes},
 	{"1#2013.06.10T22:03:49.713", &K{KZ, NONE, []time.Time{DatetimeAsTime}}, DateTimeVecBytes},
 	{"1#2013.06.10", &K{KD, NONE, []time.Time{DateAsTime}}, DateVecBytes},
@@ -45,9 +45,12 @@ var encodingTests = []struct {
 	{"2#2018.01.26D01:49:00.884361000", &K{KP, NONE, []time.Time{TimestampAsTime, TimestampAsTime}}, TimestampVectorAsBytes},
 	{"8c6b8b64-6815-6084-0a3e-178401251b68", &K{-UU, NONE, uuid.UUID{0x8c, 0x6b, 0x8b, 0x64, 0x68, 0x15, 0x60, 0x84, 0x0a, 0x3e, 0x17, 0x84, 0x01, 0x25, 0x1b, 0x68}}, GUIDBytes},
 	{"0x0 sv/: 16 cut `byte$til 32", &K{UU, NONE, []uuid.UUID{{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}, {0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f}}}, GUIDVecBytes},
-	{"0D01:22:33.444555666*1+til 2", &K{KN, NONE, []time.Duration{}}, TimespanVecBytes},
-	{"`s#`a`b!2 3", &K{XD, SORTED, Dict{&K{KS, SORTED, []string{"a", "b"}}, &K{KI, NONE, []int32{2, 3}}}}, SortedDictBytes},
-	{"`s#([]a:enlist 2;b:enlist 3)", &K{XT, SORTED, NewTable([]string{"a", "b"}, []*K{{KI, NONE, []int32{2}}, {KI, NONE, []int32{3}}})}, SortedTableBytes},
+	{"0D01:22:33.444555666*1+til 2", &K{KN, NONE, []time.Duration{4953444555666, 9906889111332}}, TimespanVecBytes},
+	{"`s#`a`b!2 3", &K{XD, SORTED, Dict{&K{KS, SORTED, []string{"a", "b"}}, IntV([]int32{2, 3})}}, SortedDictBytes},
+	{"`s#([]a:enlist 2;b:enlist 3)", &K{XT, SORTED, Table{[]string{"a", "b"}, []*K{{KI, PARTED, []int32{2}}, IntV([]int32{3})}}}, SortedTableBytes},
+	{"-8!sums", &K{KSCAN, NONE, &K{KFUNCBP, NONE, byte(1)}}, []byte{0x01, 0x00, 0x00, 0x00, 0x0b, 0x00, 0x00, 0x00, 0x6c, 0x66, 0x01}},
+	{"-8!2000.01.01", Date(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)), []byte{0x01, 0x00, 0x00, 0x00, 0x0d, 0x00, 0x00, 0x00, 0xf2, 0x00, 0x00, 0x00, 0x00}},
+	{"-8!(\"ab\"@0x40\\:)", &K{KCOMP, NONE, []*K{{KPROJ, NONE, []*K{{KFUNCBP, NONE, uint8(18)}, {KC, NONE, "ab"}}}, {KEACHLEFT, NONE, &K{-KG, NONE, byte(0x40)}}}}, []byte{0x01, 0x00, 0x00, 0x00, 0x1f, 0x00, 0x00, 0x00, 0x69, 0x02, 0x00, 0x00, 0x00, 0x68, 0x02, 0x00, 0x00, 0x00, 0x66, 0x12, 0x0a, 0x00, 0x02, 0x00, 0x00, 0x00, 0x61, 0x62, 0x6f, 0xfc, 0x40}},
 }
 
 func TestEncoding(t *testing.T) {
@@ -55,7 +58,6 @@ func TestEncoding(t *testing.T) {
 		t.Skip("skipping test in short mode.")
 	}
 	for _, tt := range encodingTests {
-		// fmt.Println(tt.desc)
 		buf := new(bytes.Buffer)
 		err := Encode(buf, ASYNC, tt.input)
 		if err != nil {
